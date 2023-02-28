6b8486aaf7216cb14d6e1b8d9df8c6cbed6ce206
leonardoserra <serra.leonardo92@gmail.com>
2023-02-28 16:04:53 +0100

    creata array delle immagini getElementByClassName

--- js/main.js
+++ js/main.js
@@ -5,11 +5,25 @@ let imagesListContent = "";
 
 console.log(imagesArray);
 for(let i = 0; i < imagesArray.length; i++){
-    let imagesListFiller = `<div class="image-wrapper">
-                                <img class="image" src="./img/0${imagesArray[i]}.webp" />
-                            </div>`;
+    let imagesListFiller = `<div class="image-wrapper"><img class="image" src="./img/0${imagesArray[i]}.webp" /></div>`;
     imagesListContent += imagesListFiller;
 }
-imagesList.innerHTML = imagesListContent;
 
+imagesList.innerHTML = imagesListContent;
 console.log(imagesListContent);
+
+const imageGroup = document.getElementsByClassName('image-wrapper');
+
+console.log(imageGroup);
+
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+
+// addEventListener.next('click',
+//     function(){
+
+
+//     }
+// )
+
+
